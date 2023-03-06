13029c53a490d0fb1e6d2dd561b9b97226d73bfa
Andrey Yurov <a.yurov6@gmail.com>
2023-03-07 01:42:23 +0700

    Restored soft deleted models

--- routes/web.php
+++ routes/web.php
@@ -33,4 +33,8 @@ Route::middleware('auth')->group(function () {
 Route::resource('/notes', NoteController::class)->middleware(['auth']);
 
 Route::get('/trashed', [TrashedNoteController::class, 'index'])->middleware('auth')->name('trashed.index');
+Route::get('/trashed/{note}', [TrashedNoteController::class, 'show'])
+    ->withTrashed()
+    ->middleware('auth')->name('trashed.show');
+
 require __DIR__.'/auth.php';
