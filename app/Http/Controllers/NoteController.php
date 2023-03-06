9aa15e8c5889f0f7b69708cfc24a1aeb3af8fee3
Andrey Yurov <a.yurov6@gmail.com>
2023-03-06 23:32:59 +0700

    Used Eloquent relatinships

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -14,7 +14,9 @@ class NoteController extends Controller
      */
     public function index()
     {
-        $notes = Note::where('user_id', Auth::id())->latest('updated_at')->paginate(5);
+        // $notes = Note::where('user_id', Auth::id())->latest('updated_at')->paginate(5);
+        // $notes = Auth::user()->notes()->latest('updated_at')->paginate(5);
+        $notes = Note::whereBelongsTo(Auth::user())->latest('updated_at')->paginate(5);
         return view('notes.index')->with('notes', $notes);
     }
 
@@ -36,9 +38,8 @@ class NoteController extends Controller
             'text' => 'required'
         ]);
 
-        Note::create([
+        Auth::user()->notes()->create([
             'uuid' => Str::uuid(),
-            'user_id' => Auth::id(),
             'title' => $request->title,
             'text' => $request->text 
         ]);
@@ -50,7 +51,7 @@ class NoteController extends Controller
      */
     public function show(Note $note)
     {
-        if ($note->user_id != Auth::id()){
+        if (!$note->user->is(Auth::user())){
             return abort(403);
         }
 
@@ -62,7 +63,7 @@ class NoteController extends Controller
      */
     public function edit(Note $note)
     {
-        if ($note->user_id != Auth::id()){
+        if (!$note->user->is(Auth::id())){
             return abort(403);
         }
         
@@ -74,7 +75,7 @@ class NoteController extends Controller
      */
     public function update(Request $request, Note $note)
     {
-        if ($note->user_id != Auth::id()){
+        if (!$note->user->is(Auth::user())){
             return abort(403);
         }
 
@@ -96,7 +97,7 @@ class NoteController extends Controller
      */
     public function destroy(Note $note)
     {  
-        if ($note->user_id != Auth::id()){
+        if (!$note->user->is(Auth::user())){
             return abort(403);
         }
 
--- app/Models/Note.php
+++ app/Models/Note.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Note extends Model
 {
@@ -14,5 +15,9 @@ class Note extends Model
     public function getRouteKeyName()
     {
         return 'uuid';
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
     }
 }
